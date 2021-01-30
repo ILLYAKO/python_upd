921555128da44a0c35f555f854458b8fb7cec84d
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2021-01-29 19:19:54 -0600

    2021-01-29

--- Ch4/files_start.py
+++ Ch4/files_start.py
@@ -3,19 +3,37 @@
 #
 
 def main():  
-  # Open a file for writing and create it if it doesn't exist
+  # # # Open a file for writing and create it if it doesn't exist
+  # # f = open("textfile.txt", "w+")
+
+  # # Open the file for appending text to the end
+  # f = open("textfile.txt", "a")
+
+
+  # # write some lines of data to the file
+  # for i in range(10):
+  #   # f.write("This is line " + str(i) + "\r\n")
+  #   f.write("This is line " + str(i) + "\r")
 
   
-  # Open the file for appending text to the end
-
-
-  # write some lines of data to the file
-
-  
-  # close the file when done
-
+  # # close the file when done
+  # f.close()
   
   # Open the file back up and read the contents
+  f = open("textfile.txt", "r")
+
+  # # read file
+  # if f.mode == 'r':
+  #   contents = f.read()
+  #   print(contents)
+
+  # read line by line
+  if f.mode == 'r':
+    fl = f.readline()
+    for x in fl:
+      print (x)
+      print("-*-*-*-")
+
 
     
 if __name__ == "__main__":
